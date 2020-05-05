8437d9bfcac55d13fefb65baa3827c59a89ddea3
Pedro Viado <pedrohenrique9592@gmail.com>
2020-05-04 22:40:56 -0300

    Ajustar designer (pendente)

--- mobile/src/pages/Register/index.js
+++ mobile/src/pages/Register/index.js
@@ -1,116 +1,164 @@
-import React from 'react';
-import { View, Text, TouchableOpacity, ScrollView } from 'react-native';
-import { Scope } from '@unform/core';
-import styles from "./styles";
+import React, { useRef } from 'react';
 import { useNavigation } from '@react-navigation/native';
+
+import {
+    View,
+    Text,
+    TouchableOpacity,
+    ScrollView,
+    ImageBackground,
+} from 'react-native';
 import Input from '../../components/Form/Input';
+
+import global from '../../global';
+import styles from './styles';
+
 import { Form } from '@unform/mobile';
+import { Scope } from '@unform/core';
+
+
 
 export default function Register() {
+
     const navigation = useNavigation();
+    const formRegister = useRef(null);
 
     function navigateToRegister() {
         navigation.navigate('RegisterOng');
+    }
+    function handleSubmit(data) {
+        console.log(data);
     }
 
     return (
 
         <View style={styles.container}>
-            <ScrollView showsVerticalScrollIndicator={false}>
-                <Text style={styles.title}>Cadastrar Usuário</Text>
+            <ImageBackground source={require('../../assets/background-register.jpg')} style={global.backgroundImage}>
 
-                <View>
-                    <Form>
-                        <Input style={styles.input}
-                            placeholder="Nome"
-                            autoCorrect={false} />
-                        <Input style={styles.input}
-                            placeholder="Sobrenome"
-                            autoCorrect={false}
-                        />
-                        <Input style={styles.input}
-                            placeholder="Email"
-                            autoCorrect={false}
-                        />
-                        <Input style={styles.input}
-                            secureTextEntry={true}
-                            placeholder="Senha"
-                            autoCorrect={false}
-                        />
-                        <Input style={styles.input}
-                            keyboardType="number-pad"
-                            placeholder="Telefone"
-                            autoCorrect={false}
-                        />
-                        <Input style={styles.input}
-                            keyboardType="number-pad"
-                            placeholder="CPF"
-                            autoCorrect={false}
-                        />
+                <ScrollView showsVerticalScrollIndicator={false}>
 
-                        <View>
+                    <View style={global.container}>
+                        <Text style={styles.title}>Cadastrar Usuário</Text>
+                    </View>
+
+                    <Form ref={formRegister} onSubmit={handleSubmit}>
+                        <View style={global.container}>
+                            <Input
+                                style={global.input}
+                                name="firstName"
+                                placeholder="Nome"
+                                autoCorrect={false}
+                            />
+                            <Input
+                                style={global.input}
+                                name="lastName"
+                                placeholder="Sobrenome"
+                                autoCorrect={false}
+                            />
+                            <Input
+                                style={global.input}
+                                name="email"
+                                placeholder="Email"
+                                autoCorrect={false}
+                            />
+                            <Input
+                                style={global.input}
+                                name="password"
+                                placeholder="Senha"
+                                autoCorrect={false}
+                                secureTextEntry={true}
+                            />
+                            <Input
+                                style={global.input}
+                                name="phone"
+                                placeholder="Telefone"
+                                keyboardType="number-pad"
+                                autoCorrect={false}
+                            />
+                            <Input
+                                style={global.input}
+                                name="document"
+                                placeholder="CPF"
+                                keyboardType="number-pad"
+                                autoCorrect={false}
+                            />
+                        </View>
+
+                        <View style={global.container}>
                             <Text style={styles.subtitle}>Endereço</Text>
+                        </View>
 
-                            <Scope>
-                                <View style={styles.formEndereco}>
-
-                                    <Input style={styles.inputCep}
-                                        keyboardType="number-pad"
+                        <Scope>
+                            <View style={global.container}>
+                                <View style={styles.containerCepButton}>
+                                    <Input
+                                        style={styles.inputCep}
+                                        name="cep"
                                         placeholder="CEP"
+                                        keyboardType="number-pad"
                                         autoCorrect={false}
                                     />
                                     <TouchableOpacity
-                                        style={styles.botaoVerificar}
+                                        style={styles.checkButton}
                                         onPress={() => { }}
                                     >
                                         <Text style={styles.botaoText}>Verificar</Text>
                                     </TouchableOpacity>
-
                                 </View>
 
-                                <View style={styles.formEndereco}>
-                                    <Input style={styles.inputCidade}
-                                        editable={false}
+                                <View style={styles.containerCityUf}>
+                                    <Input
+                                        style={styles.inputCity}
+                                        name="city"
                                         placeholder="Cidade"
                                         autoCorrect={false}
-                                    />
-                                    <Input style={styles.inputUf}
                                         editable={false}
+                                    />
+                                    <Input
+                                        style={styles.inputUf}
+                                        name="uf"
                                         placeholder="UF"
                                         autoCorrect={false}
+                                        editable={false}
                                     />
                                 </View>
 
-                                <Input style={styles.inputBairro}
-                                    editable={false}
+                                <Input
+                                    style={styles.inputNeighborhood}
+                                    name="neighborhood"
                                     placeholder="Bairro"
                                     autoCorrect={false}
+                                    editable={false}
                                 />
 
-                                <View style={styles.formEndereco}>
-                                    <Input style={styles.inputComplemento}
+                                <View style={styles.containerComplementNumber}>
+                                    <Input
+                                        style={styles.inputComplement}
+                                        name="complement"
                                         placeholder="Complemento"
                                         autoCorrect={false}
                                     />
-                                    <Input style={styles.inputNumero}
+                                    <Input
+                                        style={styles.inputNumber}
+                                        name="number"
                                         keyboardType="number-pad"
                                         placeholder="Número"
                                         autoCorrect={false}
                                     />
                                 </View>
-                            </Scope>
+                            </View>
+                        </Scope>
 
-                            <TouchableOpacity
-                                style={styles.botao}
-                                onPress={() => { navigateToRegister() }}
-                            >
-                                <Text style={styles.botaoText}>Cadastrar</Text>
-                            </TouchableOpacity>
+                        <TouchableOpacity
+                            style={styles.button}
+                            onPress={() => { navigateToRegister() }}
+                        >
+                            <Text style={styles.botaoText}>Cadastrar</Text>
+                        </TouchableOpacity>
 
-                        </View>
                     </Form>
-                </View>
-            </ScrollView>
+                </ScrollView>
+            </ImageBackground>
         </View>
     );
 }
--- mobile/src/pages/Register/styles.js
+++ mobile/src/pages/Register/styles.js
@@ -1,7 +1,7 @@
 import { StyleSheet } from 'react-native';
 import { Platform } from 'react-native';
 import { KeyboardAvoidingView } from 'react-native';
-import  Constants  from 'expo-constants';
+import Constants from 'expo-constants';
 
 
 export default StyleSheet.create({
@@ -10,7 +10,6 @@ export default StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#E5ECE8',
     paddingTop: Constants.statusBarHeight + 20,
   },
 
@@ -38,22 +37,22 @@ export default StyleSheet.create({
     borderRadius: 3
   },
 
-  formEndereco: {
+
+  // View com Cidade e UF lado a lado
+  containerCityUf: {
     flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-between',
   },
-
   inputUf: {
-    marginTop: 12,
+    marginTop: 7,
     padding: 10,
     backgroundColor: '#f0f0f0',
     fontWeight: 'bold',
-    borderRadius: 3,
-    width: 45
+    borderRadius: 10,
+    width: '15%'
   },
-
-  inputCidade: {
+  inputCity: {
     marginTop: 7,
     padding: 10,
     backgroundColor: '#f0f0f0',
@@ -61,14 +60,13 @@ export default StyleSheet.create({
     borderRadius: 3,
     width: 247,
   },
+  // end view containerCityUf
 
-  inputBairro: {
-    marginTop: 7,
-    padding: 10,
-    backgroundColor: '#f0f0f0',
-    fontWeight: 'bold',
-    borderRadius: 3,
-    width: 300,
+  // View com CEP e Button verificar lado a lado
+  containerCepButton: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
   
   inputCep: {
@@ -76,41 +74,61 @@ export default StyleSheet.create({
     padding: 10,
     backgroundColor: '#ffff',
     fontWeight: 'bold',
-    borderRadius: 3,
-    width: 197,
+    borderRadius: 10,
+    width: 230,
   },
 
-  botaoVerificar: {
-    width: 95,
+  checkButton: {
+    width: 100,
     height: 50,
     backgroundColor: '#FF4500',
     marginTop: 7,
-    borderRadius: 4,
+    borderRadius: 10,
     alignItems: 'center',
     justifyContent: 'center'
   },
-  inputComplemento: {
+  // end view containerCepButton
+
+
+  // View com Complement e Number lado a lado
+  containerComplementNumber: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+
+  inputNeighborhood: {
+    marginTop: 7,
+    padding: 10,
+    backgroundColor: '#f0f0f0',
+    fontWeight: 'bold',
+    borderRadius: 10,
+    width: 300,
+  },
+
+
+  inputComplement: {
     marginTop: 7,
     padding: 10,
     backgroundColor: '#ffff',
     fontWeight: 'bold',
-    borderRadius: 3,
+    borderRadius: 10,
     width: 197,
   },
-  inputNumero: {
+  inputNumber: {
     marginTop: 7,
     padding: 10,
     backgroundColor: '#ffff',
     fontWeight: 'bold',
-    borderRadius: 3,
+    borderRadius: 10,
     width: 95,
   },
-  botao: {
+  button: {
     width: 300,
     height: 50,
     backgroundColor: '#FF4500',
     marginTop: 7,
-    borderRadius: 4,
+    borderRadius: 10,
     alignItems: 'center',
     justifyContent: 'center'
 
@@ -121,4 +139,3 @@ export default StyleSheet.create({
     color: '#ffff',
   },
 });
-

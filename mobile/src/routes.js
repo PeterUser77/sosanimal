fa236bfdd196f963e4cd7c866cefaab1a26b4a62
Pedro Viado <pedrohenrique9592@gmail.com>
2020-05-04 22:39:10 -0300

    Adicionar tela Init

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -1,23 +1,24 @@
 import React from 'react';
-import {createStackNavigator} from '@react-navigation/stack';
-import {NavigationContainer} from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
 
+import Init from './pages/Init';
 import Auth from './pages/Auth';
 import Register from './pages/Register';
 import RegisterOng from './pages/RegisterOng';
 
 const AppStack = createStackNavigator();
-export default function Routes(){
-    return(
+export default function Routes() {
+    return (
         <NavigationContainer>
 
-        <AppStack.Navigator screenOptions={{headerShown: false}}>
-            <AppStack.Screen name="Auth" component={Auth}/>
-            <AppStack.Screen name="Register" component={Register}/>
-            <AppStack.Screen name="RegisterOng" component={RegisterOng}/>
-        </AppStack.Navigator>
+            <AppStack.Navigator screenOptions={{ headerShown: false }}>
+                <AppStack.Screen name="Init" component={Init} />
+                <AppStack.Screen name="Auth" component={Auth} />
+                <AppStack.Screen name="Register" component={Register} />
+                <AppStack.Screen name="RegisterOng" component={RegisterOng} />
+            </AppStack.Navigator>
 
         </NavigationContainer>
     );
 }
- 
